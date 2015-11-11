ed846edc08cf044886ad9e6cd69362009f30f261
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-11 16:14:36 -0700

    added static resource test

--- test/thx/tpl/TestStaticResourceTemplate.hx
+++ test/thx/tpl/TestStaticResourceTemplate.hx
@@ -5,4 +5,14 @@ import thx.tpl.HtmlTemplate;
 
 class TestStaticResourceTemplate {
   public function new() {}
+
+  public function testStaticResource() {
+    var template = new HtmlTemplate(Resource.sample);
+    var output = template.execute(["title" => "Page Title", "content" => "Page Content"]);
+    Assert.stringContains("<title>Page Title</title>", output);
+    Assert.stringContains("<div>Page Content</div>", output);
+  }
 }
+
+@:dir("test/templates")
+class Resource implements thx.StaticResource {}
