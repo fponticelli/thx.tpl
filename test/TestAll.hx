556e4dfba14b0c7aa0cdc2d123f0a1e644e59fa3
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-11 15:06:16 -0700

    simplified test execution and added placeholder for static resource

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -1,17 +1,11 @@
-import utest.Runner;
-import utest.ui.Report;
+import utest.UTest;
 
 class TestAll {
-  public static function addTests(runner : Runner) {
-    runner.addCase(new thx.tpl.TestParser());
-    runner.addCase(new thx.tpl.TestScriptBuilder());
-    runner.addCase(new thx.tpl.TestTemplate());
-  }
-
-  public static function main() {
-    var runner = new Runner();
-    addTests(runner);
-    Report.create(runner);
-    runner.run();
-  }
+  public static function main()
+    UTest.run([
+        new thx.tpl.TestParser(),
+        new thx.tpl.TestScriptBuilder(),
+        new thx.tpl.TestStaticResourceTemplate(),
+        new thx.tpl.TestTemplate()
+      ]);
 }
---
+++ test/thx/tpl/TestStaticResourceTemplate.hx
@@ -0,0 +1,8 @@
+package thx.tpl;
+
+import utest.Assert;
+import thx.tpl.HtmlTemplate;
+
+class TestStaticResourceTemplate {
+  public function new() {}
+}
