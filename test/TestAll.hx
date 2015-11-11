274c0f0e392ce6fb3c0f21f8dcbb38d4f09be607
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-11-11 14:21:56 -0700

    minor

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -2,16 +2,16 @@ import utest.Runner;
 import utest.ui.Report;
 
 class TestAll {
-	public static function addTests(runner : Runner) {
-		runner.addCase(new thx.tpl.TestParser());
-		runner.addCase(new thx.tpl.TestScriptBuilder());
-		runner.addCase(new thx.tpl.TestTemplate());
-	}
+  public static function addTests(runner : Runner) {
+    runner.addCase(new thx.tpl.TestParser());
+    runner.addCase(new thx.tpl.TestScriptBuilder());
+    runner.addCase(new thx.tpl.TestTemplate());
+  }
 
-	public static function main() {
-		var runner = new Runner();
-		addTests(runner);
-		Report.create(runner);
-		runner.run();
-	}
+  public static function main() {
+    var runner = new Runner();
+    addTests(runner);
+    Report.create(runner);
+    runner.run();
+  }
 }
