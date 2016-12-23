fbd6f28540338bb144876e57c7c44f70020ba305
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-12-23 10:24:04 -0700

    fix doc

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
 rm thx.tpl.zip
-zip -r thx.tpl.zip hxml src test extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
+zip -r thx.tpl.zip doc/ImportTpl.hx hxml src test extraParams.hxml haxelib.json LICENSE README.md -x "*/\.*"
 haxelib submit thx.tpl.zip
