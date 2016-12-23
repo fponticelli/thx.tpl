80b2258dfc6b6df31ac9e3411b2ed8e7b87e32dd
Franco Ponticelli <franco.ponticelli@gmail.com>
2016-12-23 10:09:23 -0700

    add docs

---
+++ doc/ImportTpl.hx
@@ -0,0 +1,7 @@
+import thx.tpl.HtmlTemplate;
+import thx.tpl.Output;
+import thx.tpl.Parser;
+import thx.tpl.ParserError;
+import thx.tpl.ScriptBuilder;
+import thx.tpl.Template;
+import thx.tpl.hscript.EnhancedInterp;
